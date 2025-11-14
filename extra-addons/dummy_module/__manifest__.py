2786360429ed6c9ffb9879eb68116b2834b8692d
marcosmateos1 <marcos.mateos1@isepceu.es>
2025-11-14 16:10:18 +0100

    Update __manifest__.py

---
+++ extra-addons/dummy_module/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Dummy Module',
+    'version': '1.0',
+    'summary': 'Empty test module',
+    'author': 'Marcos Mateos',
+    'depends': [],
+    'data': [],
+    'installable': True,
+    'application': False,
+}
