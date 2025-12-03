278e54c40750ab2e92fff20022f8fb3cbc983b15
Marcos Mateos Iglesias <marcos.mateosiglesias@beca.ceu.es>
2025-12-03 20:33:12 +0100

    Añadir estructura para dummy_module

---
+++ extra-addons/dummy_module/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Dummy Module',
+    'version': '1.0',
+    'summary': 'Empty test module',
+    'author': 'Marcos Mateos',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/dummy_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
