65a1c00a88e82292cbe03b3f6a36424c6c840aaa
marcosmateos1 <marcos.mateos1@isepceu.es>
2025-12-03 20:29:59 +0100

    Update __manifest__.py

--- extra-addons/dummy_module/__manifest__.py
+++ extra-addons/dummy_module/__manifest__.py
@@ -3,8 +3,11 @@
     'version': '1.0',
     'summary': 'Empty test module',
     'author': 'Marcos Mateos',
-    'depends': [],
-    'data': [],
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/dummy_views.xml',
+    ],
     'installable': True,
     'application': False,
 }
